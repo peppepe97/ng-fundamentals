ec5c2c4d77466d329daa798b813c582f2a37c54c
giuperi <giuseppe.peri@capgemini.com>
2021-03-22 16:02:45 +0100

    various

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent implements OnInit {
+
+  @Input() event:any
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getStartTimeClass(){
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return {green: isEarlyStart, bold: isEarlyStart}
+  }
+
+}
